71b57ebe2f15e5aa06ca97c8368016c5095819fa
PhanterJR <junior.conex@gmail.com>
2021-11-17 13:51:18 -0300

    Update preloaders.py

--- phanterpwa/frontend/preloaders.py
+++ phanterpwa/frontend/preloaders.py
@@ -97,3 +97,31 @@ android = DIV(
         _class="preload-wrapper"),
     _class="phanterpwa-components-preloaders-android"
 ).jquery()
+
+
+run_points = DIV(
+    DIV(
+        DIV(
+            DIV(
+                DIV(
+                    DIV(_class="phanterpwa_run_points_one"),
+                    _class="phanterpwa_run_points phanterpwa_run_points_one_content"),
+                DIV(
+                    DIV(_class="phanterpwa_run_points_two"),
+                    _class="phanterpwa_run_points phanterpwa_run_points_two_content"),
+                DIV(
+                    DIV(_class="phanterpwa_run_points_three"),
+                    _class="phanterpwa_run_points phanterpwa_run_points_three_content"),
+                DIV(
+                    DIV(_class="phanterpwa_run_points_four"),
+                    _class="phanterpwa_run_points phanterpwa_run_points_four_content"),
+                DIV(
+                    DIV(_class="phanterpwa_run_points_five"),
+                    _class="phanterpwa_run_points phanterpwa_run_points_five_content"),
+                _class="pwa_run_points_container"
+            ),
+            _class='preloader-wrapper enabled'
+        ),
+        _class="phanterpwa-preloader-wrapper"),
+    _class="phanterpwa-components-preloaders-run_points"
+)
