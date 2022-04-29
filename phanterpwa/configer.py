a81989bcbc4705d67f1fb02a77e625b71c888dd3
PhanterJR <junior.conex@gmail.com>
2022-04-29 08:51:29 -0300

    Update configer.py

--- phanterpwa/configer.py
+++ phanterpwa/configer.py
@@ -193,7 +193,22 @@ class ProjectConfig():
                             "title"
                         ]
                         if "APP" in app_sections:
-                            cfg["BACKEND"][app_name] = {}
+                            cfg["BACKEND"][app_name] = {
+                                "default_time_user_token_expire": 7200,
+                                "default_time_user_token_expire_remember_me": 2592000,
+                                "default_time_csrf_token_expire": 4200,
+                                "default_time_temporary_password_expire": 4200,
+                                "timeout_to_resend_temporary_password_mail": 3900,
+                                "default_time_client_token_expire": 63072000,
+                                "default_time_activation_code_expire": 3600,
+                                "wait_time_to_try_activate_again": 3900,
+                                "timeout_to_resend_activation_email": 300,
+                                "timeout_to_resign": 600,
+                                "max_login_attempts": 5,
+                                "max_activation_attempts": 5,
+                                "default_time_user_token_expire": 7200,
+                                "default_time_two_factor_code_expire": 3600
+                            }
 
                             for k in required_on_ini:
                                 v = ini_app["APP"].get(k, None)
